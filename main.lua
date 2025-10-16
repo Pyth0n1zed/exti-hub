22436fb3c5d923b1f2a97027b71964250d4a4c4a
Pyth0n1zed <166272311+Pyth0n1zed@users.noreply.github.com>
2025-10-16 04:13:29 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -339,6 +339,8 @@ hrp:PivotTo(part.CFrame + Vector3.new(0,1,0))
 	end
 	for i = 1, 1000 do
 		for i,v in pairs(game.Players:GetPlayers()) do
+			if not v.Character then break end
+			if not v.Character:FindFirstChild("Humanoid") then break end
 			if character.Humanoid.Health == 0 then break end
 			if v.Character.Humanoid.Health > 0 then
 				name = v.Name
