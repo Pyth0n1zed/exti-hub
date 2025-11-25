f15e9d09a455cbdf0bdc020a4e7d180591282b24
Py <166272311+Pyth0n1zed@users.noreply.github.com>
2025-11-24 21:55:14 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -664,7 +664,6 @@ function AutoWin2()
 		end
 	end
 	task.wait(1)
-	local leastHealth = nil
 	local prevPlayer = nil
 	while task.wait() do
 		for _,v in pairs(game.Players:GetPlayers()) do
@@ -702,6 +701,7 @@ function AutoWin2()
 			elseif prevPlayer and iceCount == 0 then
 				waitTime = 2.25*(prevPlayer.Position-thrp.Position).Magnitude/1000
 			end
+			print(waitTime)
 			if waitTime < 0.7 then
 				waitTime = 0.7
 			end
