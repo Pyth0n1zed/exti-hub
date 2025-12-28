65587c63ce0535fa9301c1bc3af9dfcbc005a061
Py <166272311+Pyth0n1zed@users.noreply.github.com>
2025-12-28 16:14:22 -0600

    Update slapfarm3.lua

--- slapfarm3.lua
+++ slapfarm3.lua
@@ -1,5 +1,5 @@
 
-if not(game.PlaceId == 9426795465 or game.PlaceId == 6403373529) then
+if not(game.PlaceId == 9426795465 or game.PlaceId == 6403373529 or game.PlaceId == 124596094333302) then
 	local inMatch = false
 --local --exti =loadstring(game:HttpGet("https://raw.githubusercontent.com/Pyth0n1zed/GUI-Framework-Roblox/refs/heads/main/script.lua"))()()
 local exti = exti or {}
@@ -758,6 +758,7 @@ end
 local TARGET_PLACE = 9426795465
 local BLOCKED_PLACES = {
 	[6403373529] = true,
+	[124596094333302] = true
 	--[9426795465] = true
 }
 
