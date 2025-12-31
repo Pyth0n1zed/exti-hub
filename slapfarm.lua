7b988b6daa20d36f513e3c47f1c72f29c81c0c5c
Py <166272311+Pyth0n1zed@users.noreply.github.com>
2025-12-31 00:04:16 -0600

    Update slapfarm.lua

--- slapfarm.lua
+++ slapfarm.lua
@@ -173,8 +173,8 @@ local function CollectItemsSR(itemNames, repeatCount, amount)
                 faceTarget(handle)
                 rotateCameraTo(handle)
                 task.wait(pauseTime)
-				if ii == 1 then task.wait(3) end
-				if ii == 2 then task.wait(1) end
+				if ii == 1 then task.wait(1) end
+				if ii == 2 then task.wait() end
                 sendFKey()
                 task.wait(pauseTime)
                 sendSpaceKey()
